8722c6943234884523f2baaa43d74f4a4a0cf104
josegabrielms <josegabrielms@outlook.com>
2023-05-21 18:20:32 -0500

    To do list 1st

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+export default function Contact(){
+    return(
+        <div>
+            <h3>Desde Contacto</h3>
+            <p>Correo electrónico: josegabriel@outlook.com</p>
+            <p>Número de contacto: 317 000 0000</p>
+            <p>Dirección de residencia: Calle falsa 123</p>
+        </div>
+    )
+};
